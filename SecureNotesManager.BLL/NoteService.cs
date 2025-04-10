290de10068bfe45037c4078e62a4aa8b3e820095
Mohammad Huseynov <mohamadHuseynov@gmail.com>
2025-04-10 23:30:48 +0330

    Setup 3-tier architecture and connected UI to BLL via NoteService

---
+++ SecureNotesManager.BLL/NoteService.cs
@@ -0,0 +1,50 @@
+﻿using SecureNotesManager.Models;
+using SecureNotesManager.DAL;
+
+namespace SecureNotesManager.BLL
+{
+    public class NoteService
+    {
+        private readonly SecureNotesDbContext _context;
+
+        public NoteService()
+        {
+            _context = new SecureNotesDbContext();
+        }
+
+        public void AddNote(Note note)
+        {
+            note.CreatedAt = DateTime.Now;
+            _context.Notes.Add(note);
+            _context.SaveChanges();
+        }
+
+        public List<Note> GetAllNotes()
+        {
+            return _context.Notes.OrderByDescending(n => n.CreatedAt).ToList();
+        }
+
+        public void DeleteNote(int id)
+        {
+            var note = _context.Notes.Find(id);
+            if (note != null)
+            {
+                _context.Notes.Remove(note);
+                _context.SaveChanges();
+            }
+        }
+
+        public void UpdateNote(Note note)
+        {
+            var existing = _context.Notes.Find(note.Id);
+            if (existing != null)
+            {
+                existing.Title = note.Title;
+                existing.Content = note.Content;
+                existing.ModifiedAt = DateTime.Now;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
+
---
+++ SecureNotesManager.DAL/SecureNotesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureNotesManager.Models;
+
+namespace SecureNotesManager.DAL
+    {
+        public class SecureNotesDbContext : DbContext
+        {
+            public DbSet<Note> Notes { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer("Server=.;Database=SecureNotesDb;Trusted_Connection=True;TrustServerCertificate=True;");
+                }
+            }
+        }
+    }
+
+
+
+
